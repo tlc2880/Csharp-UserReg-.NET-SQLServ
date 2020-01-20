06df6786c4dcfb89dded8b864fc14b4f0ead172b
Tommy Cao <gevh2880@gmail.com>
2020-01-19 16:36:25 -0800

    first commit

---
+++ User Registration/Form1.cs
@@ -0,0 +1,69 @@
+﻿//  Name: Tommy Cao
+//  Date: 07/11/15
+//  Title: C# - User Registration or Sign Up Form with SQL Server
+//  Description:  Implementation of user registration or sign up form in C# windows form application.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace User_Registration
+{
+    public partial class Form1 : Form
+    {
+        string connectionString = @"Data Source = CAOPC-19; Initial Catalog = UserRegistrationDB; Integrated Security=True;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "" || txtPassword.Text == "")
+                MessageBox.Show("please fill mandatory fields");
+            else if (txtPassword.Text != txtConfirmPassword.Text)
+                MessageBox.Show("Password do not match");
+            else
+            {
+                using (SqlConnection sqlCon = new SqlConnection(connectionString))
+                {
+                    sqlCon.Open();
+                    SqlCommand sqlCmd = new SqlCommand("UserAdd", sqlCon);
+                    sqlCmd.CommandType = CommandType.StoredProcedure;
+                    sqlCmd.Parameters.AddWithValue("@FirstName", txtFirstName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@LastName", txtLastName.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Contact", txtContact.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Address", txtAddress.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Username", txtUsername.Text.Trim());
+                    sqlCmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+                    sqlCmd.ExecuteNonQuery();
+                    MessageBox.Show("Registration is successfull");
+                    Clear();
+
+                }
+            }
+        }
+        void Clear()
+        {
+            txtFirstName.Text = txtLastName.Text = txtContact.Text = txtAddress.Text = txtUsername.Text = txtPassword.Text =txtConfirmPassword.Text= "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ User Registration/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Name: Tommy Cao
+//  Date: 07/11/15
+//  Title: C# - User Registration or Sign Up Form with SQL Server
+//  Description:  Implementation of user registration or sign up form in C# windows form application.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace User_Registration
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
